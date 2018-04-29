22a084469e479bf14bd5b371882680c782e8fa38
kirillurazov <Semen7empty@gmail.com>
2018-04-29 15:16:39 +0700

    test

--- tdd/tdd/Form1.Designer.cs
+++ tdd/tdd/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(390, 345);
+            this.ClientSize = new System.Drawing.Size(337, 258);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
